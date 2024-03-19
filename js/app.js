3a8c48d1790e144b8e184a8f273ab2bb8d6752f8
Giulio998 <giulio_favia@hotmail.it>
2024-03-19 14:02:41 +0100

    Add snack 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+/* SNACK 1 */
+
 const guests = [
     'Brad Pitt',
     'Johnny Depp',
@@ -17,3 +19,43 @@ const guestsList = guests.map((element, index) => {
     }
 })
 console.log(guestsList)
+
+
+
+/* SNACK 2 */
+const students = [
+    { id: 213, name: 'Marco della Rovere', vote: 78 },
+    { id: 110, name: 'Paola Cortellessa  ', vote: 96 },
+    { id: 250, name: 'Andrea Mantegna', vote: 48 },
+    { id: 145, name: 'Gaia Borromini', vote: 74 },
+    { id: 196, name: 'Luigi Grimaldello', vote: 68 },
+    { id: 102, name: 'Piero della Francesca', vote: 50 },
+    { id: 120, name: 'Francesca da Polenta', vote: 84 },
+];
+console.log(students);
+
+// # STUDENTI CON VOTI SUPERIORI A 70
+const getVote = (arr, min) => {
+    return arr.filter((item) => (item.vote > min));
+}
+console.log(getVote(students, 70))
+
+// # STUDENTI ID MAGGIORE DI 120
+const getId = (arr, min) => {
+    return arr.filter((item) => (item.id > min));
+}
+console.log(getId(students, 120));
+
+const getIdAndVote = (arr, val1, val2) => {
+    return arr.filter((item) => (item.id > val1 && item.vote > val2));
+};
+console.log(getIdAndVote(students, 120, 70))
+
+const capitalize = (phrase) => {
+    return phrase.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
+};
+
+(getIdAndVote(students,120,70)).forEach((element) => {
+    element.name = capitalize(element.name);
+    console.log(`${element.name} (id: ${element.id}), con un voto di: ${element.vote}`);
+});
